e998199dcfa0fcf3c0359fdc4aac1e1fed6c90ca
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-11-08 16:24:43 -0500

    feat: update health endpoint

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 version = "1.0"
 
 
-@app.get("/microservice/health")
+@app.get("/generative-ai/health")
 async def health():
-    return {"status": "OK Python"}
+    return {"status": "OK Generative AI"}
 
 @app.exception_handler(ApiError)
 async def api_error_exception_handler(request: Request, exc: ApiError):
